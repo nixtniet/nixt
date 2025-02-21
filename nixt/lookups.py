24e056d73ade877016a41b3858ec67d4a7550d85
Bart Thate <bthate@dds.nl>
2025-02-21 20:55:28 +0100

    initial import

---
+++ nixt/lookups.py
@@ -0,0 +1,30 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "nixt.modules.irc",
+    "dne": "nixt.modules.tdo",
+    "dpl": "nixt.modules.rss",
+    "err": "nixt.modules.err",
+    "exp": "nixt.modules.rss",
+    "flt": "nixt.modules.flt",
+    "fnd": "nixt.modules.fnd",
+    "imp": "nixt.modules.rss",
+    "log": "nixt.modules.log",
+    "mod": "nixt.modules.mod",
+    "mre": "nixt.modules.irc",
+    "nme": "nixt.modules.rss",
+    "pwd": "nixt.modules.irc",
+    "req": "nixt.modules.req",
+    "res": "nixt.modules.rss",
+    "rss": "nixt.modules.rss",
+    "slg": "nixt.modules.slg",
+    "syn": "nixt.modules.rss",
+    "tdo": "nixt.modules.tdo",
+    "thr": "nixt.modules.thr",
+    "upt": "nixt.modules.upt",
+    "ver": "nixt.modules.upt"
+}
