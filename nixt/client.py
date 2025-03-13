e1349f443cda6956ff9b79691828d714a8aef703
Bart Thate <nixtniet@gmail.com>
2025-03-13 20:20:09 +0100

    add client

---
+++ nixt/client.py
@@ -0,0 +1,65 @@
+# This file is placed in the Public Domain.
+
+
+"client"
+
+
+from .fleet  import Fleet
+from .object import Default
+from .run    import Reactor
+
+
+import threading
+import time
+
+
+class Client(Reactor):
+
+    def __init__(self):
+        Reactor.__init__(self)
+        Fleet.add(self)
+
+    def announce(self, txt) -> None:
+        pass
+
+    def raw(self, txt) -> None:
+        raise NotImplementedError("raw")
+
+    def say(self, channel, txt) -> None:
+        self.raw(txt)
+
+
+class Event(Default):
+
+    def __init__(self):
+        Default.__init__(self)
+        self._ready = threading.Event()
+        self._thr   = None
+        self.ctime  = time.time()
+        self.result = {}
+        self.type   = "event"
+        self.txt    = ""
+
+    def display(self) -> None:
+        Fleet.display(self)
+
+    def done(self) -> None:
+        self.reply("ok")
+
+    def ready(self) -> None:
+        self._ready.set()
+
+    def reply(self, txt) -> None:
+        self.result[time.time()] = txt
+
+    def wait(self) -> None:
+        self._ready.wait()
+        if self._thr:
+            self._thr.join()
+
+
+def __dir__():
+    return (
+        'Client',
+        'Event'
+    )
