1f360a47f53a387d2de52a3572efdf9d7df9fbf0
Bart Thate <nixtniet@gmail.com>
2025-03-14 22:49:55 +0100

    add lookup

---
+++ nixt/lookup.py
@@ -0,0 +1,53 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "err": "err",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "man": "man",
+    "mod": "mod",
+    "mre": "irc",
+    "nme": "rss",
+    "now": "mdl",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt"
+}
+
+
+MD5 = {
+    "err": "a03975196578a5b9b7fb455a637f465a",
+    "flt": "f56c2016cad990bb24e448ec2ef861f0",
+    "fnd": "dc9e99e127b17b54eac34e1dfae36b17",
+    "irc": "6b81ced3cd172be85968fa26605a958b",
+    "log": "6100d6513bb84318b91693ee3ae2791b",
+    "man": "aa9c5df53602a03aba8ee29a38460994",
+    "mdl": "256e959ac9fbd75c3e1b9a07bbdcc697",
+    "mod": "82d9b50b3a2a446fbb857de7192b8f28",
+    "req": "a0013d5e8582b1cb56854d17e9c66a95",
+    "rss": "88c45567cda948c8203a182fc46cbd9a",
+    "slg": "01ffe5fb93617a6a870882e074155846",
+    "tdo": "453b653e8ce2d39a04719a31b0d9411d",
+    "thr": "f413582c20c0adec4ee03d2f1fb21054",
+    "tmr": "863f49c3a5a816c24940c55a7c2e7c6d",
+    "upt": "67c0f6e5c6665539424c128027f31b96",
+}
