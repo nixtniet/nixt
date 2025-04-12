6caaef0cd2301f8644a07cbda71ddb9bb6ec2549
Bart Thate <nixtniet@gmail.com>
2025-04-12 17:12:06 +0200

    over to test

--- nixt/__init__.py
+++ nixt/__init__.py
@@ -8,3 +8,29 @@ from nixt        import object, json
 from nixt.disk   import read,write
 from nixt.object import *
 from nixt.json   import *
+
+
+def __dir__():
+    return (
+        'Decoder',
+        'Encoder',
+        'Object',
+        'construct',
+        'dump',
+        'dumps',
+        'hook',
+        'items',
+        'keys',
+        'load',
+        'loads',
+        'object',
+        'read',
+        'update',
+        'values',
+        'write'
+    )
+
+
+__all__ = __dir__()
+
+    
