b145f8c1abe387f01d18fb9beb7c376cc28d6bab
Bart Thate <bthate@dds.nl>
2025-04-18 23:11:18 +0200

    add package

---
+++ nixt/modules/tbl.py
@@ -0,0 +1,47 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dne": "tdo",
+    "dpl": "rss",
+    "err": "err",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "mod": "mod",
+    "mre": "irc",
+    "nme": "rss",
+    "pwd": "irc",
+    "rem": "rss",
+    "res": "rss",
+    "rss": "rss",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt",
+    "ver": "ver"
+}
+
+
+MD5 = {
+    "err": "2aca6bd937ceb5aa2b4233f1700804b1",
+    "flt": "f2327e29127648c9c5becc74124c2103",
+    "fnd": "e26071301327553649feccd6548c22bb",
+    "irc": "3aa836bbfdd008a540f3e05b2aba4acd",
+    "log": "4a6034537510f13a5bd28fc6f163361a",
+    "mod": "82d9b50b3a2a446fbb857de7192b8f28",
+    "rss": "fb5e5496a50753816b19874d3f0944ca",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "b945196f271f95087b65e502653ece5d",
+    "thr": "fa46c4611745eea93cb9b486499c1d02",
+    "tmr": "baec07e6af31dc5222efe3e95823fc81",
+    "upt": "8e9bdb977edc8081dc6a7b7d10e15aec",
+    "ver": "ba6cfe6e26cbaff110fb7afa356c5a7f",
+}
