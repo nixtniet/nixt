5035ad08640e6ecda2ed58483424f3e79b91112a
Bart Thate <bthate@dds.nl>
2025-05-24 23:50:20 +0200

    add client

---
+++ nixt/client.py
@@ -0,0 +1,88 @@
+# This file is placed in the Public Domain.
+
+
+"client"
+
+
+from threading import RLock
+
+
+from .event   import Event
+from .handler import Handler
+
+
+lock = RLock()
+
+
+class Client(Handler):
+
+    def __init__(self):
+        Handler.__init__(self)
+        Fleet.add(self)
+
+    def announce(self, txt: str) -> None:
+        pass
+
+    def raw(self, txt: str) -> None:
+        raise NotImplementedError("raw")
+
+    def say(self, channel: str, txt: str) -> None:
+        self.raw(txt)
+
+
+class Fleet:
+
+    clients = {}
+
+    @staticmethod
+    def add(clt: Client):
+        Fleet.clients[repr(clt)] = clt
+
+    @staticmethod
+    def all():
+        return sorted(Fleet.clients.values())
+
+    @staticmethod
+    def announce(txt: str):
+        for clt in Fleet.clients.values():
+            clt.announce(txt)
+
+    @staticmethod
+    def display(evt: Event):
+        with lock:
+            clt = Fleet.get(evt.orig)
+            for tme in sorted(evt.result):
+                clt.say(evt.channel, evt.result[tme])
+            evt.ready()
+
+    @staticmethod
+    def first() -> Client:
+        clt =  list(Fleet.clients.values())
+        res = None
+        if clt:
+            res = clt[0]
+        return res
+
+    @staticmethod
+    def get(orig: str) -> Client:
+            return Fleet.clients.get(orig, None)
+
+    @staticmethod
+    def say(orig: str, channel: str, txt: str) -> None:
+        clt = Fleet.get(orig)
+        if clt:
+            clt.say(channel, txt)
+
+    @staticmethod
+    def wait() -> None:
+        for clt in Fleet.clients.values():
+            if "wait" in dir(clt):
+                clt.wait()
+
+
+
+def __dir__():
+    return (
+        'Client',
+        'Fleet'
+    )
