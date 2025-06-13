3cc6b38055f8ced5acdf93b39fa0c1939de5d9ef
Bart Thate <bthate@dds.nl>
2025-06-13 07:03:16 +0200

    add modules

--- nixt/modules/tbl.py
+++ nixt/modules/tbl.py
@@ -35,20 +35,20 @@ NAMES = {
 
 
 MD5 = {
-    "dbg": "24c8f1fc6d3fd57186e3a878ab878b4b",
+    "dbg": "e1e3887b18d568e30459cb42cbd4b80c",
     "err": "fddabb7f6ea2e2fd55992fccc8738fe0",
     "flt": "9fbbbeb19efe8f50f01831418fdc94a7",
     "fnd": "d19c8cc42b9b72454f3b9aaaf8613603",
-    "irc": "44879fea58ea51bd465775e201ad87fc",
+    "irc": "5be8891292e44d9b2a09baa432d03532",
     "log": "b4b3b867ef1f40df485bc5bbf304fd0b",
     "mod": "82d9b50b3a2a446fbb857de7192b8f28",
     "req": "a0013d5e8582b1cb56854d17e9c66a95",
-    "rss": "2f6d48fab472ed7558f26a39295d308b",
+    "rss": "197d7a9ef6a6372926d8780bdb70ed4a",
     "slg": "99648f4d801c2356bbe18abef05e1a4b",
     "tbl": "d41d8cd98f00b204e9800998ecf8427e",
     "tdo": "15caf95bf5556e7286f7b2a64eee19ce",
     "thr": "8ca0f9ca153fd41a4ca2dbe81e68e466",
-    "tmr": "689b641b676e91bb94ca29b248a5a532",
+    "tmr": "6db7bb9f5734ccb1d82d53fcc087f110",
     "upt": "5524d8891950db0f8cbdeac67f8d5449",
     "ver": "ba6cfe6e26cbaff110fb7afa356c5a7f",
 }
