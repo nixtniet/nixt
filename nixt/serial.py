e7d6ebe6672e19c5eeda66411d234f6829a9bea7
Bart Thate <bthate@dds.nl>
2025-06-25 02:32:40 +0200

    use Encoder class

--- nixt/serial.py
+++ nixt/serial.py
@@ -10,29 +10,31 @@ import json
 from .object import Object, construct
 
 
-def default(o):
-    if isinstance(o, dict):
-        return o.items()
-    if issubclass(type(o), Object):
-        return vars(o)
-    if isinstance(o, list):
-        return iter(o)
-    try:
-        return json.JSONEncoder.default(self, o)
-    except TypeError:
-        try:
+class Encoder(json.JSONEncoder):
+
+    def default(self, o):
+        if isinstance(o, dict):
+            return o.items()
+        if issubclass(type(o), Object):
             return vars(o)
+        if isinstance(o, list):
+            return iter(o)
+        try:
+            return json.JSONEncoder.default(self, o)
         except TypeError:
-            return repr(o)
+            try:
+                return vars(o)
+            except TypeError:
+                return repr(o)
 
 
 def dump(obj, fp, *args, **kw):
-    kw["default"] = default
+    kw["cls"] = Encoder
     json.dump(obj, fp, *args, **kw)
 
 
 def dumps(obj, *args, **kw):
-    kw["default"] = default
+    kw["cls"] = Encoder
     return json.dumps(obj, *args, **kw)
 
 
