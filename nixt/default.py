97a86529d46b3f9db95c30846b8907ee4bc6eb25
Bart Thate <bthate@dds.nl>
2025-06-25 02:44:38 +0200

    add default

---
+++ nixt/default.py
@@ -0,0 +1,21 @@
+# This file is placed in the Public Domain.
+
+
+"default return values"
+
+
+from .object import Object
+
+
+class Default(Object):
+
+    def __getattr__(self, key):
+        if key not in self:
+            setattr(self, key, "")
+        return self.__dict__.get(key, "")
+
+
+def __dir__():
+    return (
+        'Default',
+    )
