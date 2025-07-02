10a2b51185dbbd76174bfbb7fbf6f85ff93afc1f
Bart Thate <bthate@dds.nl>
2025-07-02 07:29:01 +0200

    add mods

---
+++ mods/tbl.py
@@ -0,0 +1,51 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dne": "tdo",
+    "dpl": "rss",
+    "err": "err",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "mod": "mod",
+    "mre": "irc",
+    "nme": "rss",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt",
+    "ver": "ver"
+}
+
+
+MD5 = {
+    "err": "77514c7cf6fec0c2b5ef5f47d654da96",
+    "flt": "b1c1c78d82a46b44c415a03066993753",
+    "fnd": "07ba793454b7d509ce1ef410c97b2a37",
+    "irc": "38ba593cf1b7ff23cb55d31ababf49e4",
+    "log": "d79dab10b3f9a86f7cad8df2e600241e",
+    "mod": "82d9b50b3a2a446fbb857de7192b8f28",
+    "req": "a0013d5e8582b1cb56854d17e9c66a95",
+    "rss": "7e8ce6a1b8be8fee9247f8a899704489",
+    "slg": "99648f4d801c2356bbe18abef05e1a4b",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "99de81b52c4b5b0271eb353ba2d606e7",
+    "thr": "7a8c9bf416d6101f90e88348de91814d",
+    "tmr": "5306ab122e533a849e3c9bbcb8893c54",
+    "upt": "573ec8c5452fc1dedbbe99257f42fc6d",
+    "ver": "a8a0d974232df025ed4941c6b65d6a34",
+}
