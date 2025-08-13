c647b69fa0f55f6f95e5e6a7e8340d842fe29a46
Bart Thate <bthate@dds.nl>
2025-08-13 19:01:40 +0200

    add nixt.modules

---
+++ nixt/modules/tbl.py
@@ -0,0 +1,55 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "brk": "dbg",
+    "cfg": "irc",
+    "dbg": "dbg",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "eml": "mbx",
+    "exp": "rss",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "ls": "lst",
+    "mbx": "mbx",
+    "mre": "irc",
+    "nme": "rss",
+    "now": "mdl",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt"
+}
+
+
+MD5 = {
+    "dbg": "6cbdbeab1547c0936b51315fd6cd6b36",
+    "fnd": "faea9eabacb5598d317051b3a1abd125",
+    "irc": "20ccb56f1f10b274856dc8294b15c05c",
+    "log": "4fd9411406c1a7a7a49c6f606532fa29",
+    "lst": "7d6c457036a97fc7bbfe948432310553",
+    "mbx": "a3d704af9b725dd0a165bbb3a3e08d38",
+    "mdl": "5eb506f6925fba89c1dfb26d4dfad1b8",
+    "req": "793704d64443c4f19512924e294943ea",
+    "rss": "78a1ca0b5d2dcfc59694e71b312a2a84",
+    "rst": "dd2b6e4c562f430ac8b91de48b3d4c68",
+    "slg": "0b71453a0d4dd3b21618c7df3c8cd6a4",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "817d6286c93d68b49fcc1e44bc673383",
+    "thr": "808977342fed5dd5e36bb2d237eaabb1",
+    "tmr": "bdb38d73957f44c425fdc86b14721120",
+    "upt": "4e396edd146ac01b0c60258adaceb810",
+}
