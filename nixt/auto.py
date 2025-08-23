fe42802afaf7487609f548edf8358cc048e8c6eb
Bart Thate <bthate@dds.nl>
2025-08-23 17:43:06 +0200

    add auto contruct

---
+++ nixt/auto.py
@@ -0,0 +1,24 @@
+# This file is placed in the Public Domain.
+
+
+"auto construct"
+
+
+from .object import Object
+
+
+class Auto(Object):
+
+    def __getattr__(self, key):
+        if key not in self:
+            setattr(self, key, "")
+        return self.__dict__.get(key, "")
+
+
+"interface"
+
+
+def __dir__():
+    return (
+        'Auto',
+    )
