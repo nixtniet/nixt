d5056416b4db57de332e8f33ea8dc9524dedfe7a
Bart Thate <bthate@dds.nl>
2025-08-27 05:50:56 +0200

    update table

--- mods/tbl.py
+++ mods/tbl.py
@@ -1,7 +1,6 @@
 # This file is placed in the Public Domain.
 
 
-"lookup tables"
 
 
 NAMES = {
@@ -29,15 +28,15 @@ NAMES = {
 
 
 MD5 = {
-    "fnd": "9e58d9a32e833037178b35daa7510e90",
-    "irc": "1ba37f2bafad9bbde40a9efeb91e54ec",
-    "log": "830d53d78dba7ec8249b21c1acd0c288",
+    "fnd": "241678a3401e3c146e4da7401766a4d3",
+    "irc": "7ea67f8eb67b63c4166b2fae0e6b7983",
+    "log": "b464f6447b26c3cf170a4f4cff7de772",
     "req": "793704d64443c4f19512924e294943ea",
-    "rss": "2b2671baa887aa193ba9cd10cb740c08",
+    "rss": "fd6c0baa2bc4a01ac8c0b9aa2e4103ae",
     "slg": "0b71453a0d4dd3b21618c7df3c8cd6a4",
     "tbl": "d41d8cd98f00b204e9800998ecf8427e",
-    "tdo": "c9af488c85f46829a12be8043de90760",
+    "tdo": "f9d746810ca19240f712004ef501b9c9",
     "thr": "7e6bd817fd8df35820e0d2aea4dc5591",
-    "tmr": "a89ae7faf83aa8817c53770df8b6c81d",
+    "tmr": "6fff3ea58bc0610e08773091ea31255b",
     "upt": "ceeafab9201ee2bba7289aeeae355acf",
 }
