d34a1b8d0e6776d015dd12887f8e6b4810dba860
Bart Thate <bthate@dds.nl>
2025-08-30 00:32:08 +0200

    update packaget

--- nixt/__init__.py
+++
@@ -1,8 +0,0 @@
-# This file is placed in the Public Domain.
-
-
-__doc__ = __name__.upper()
-
-
-from .object import *
-from .serial import *
--- nixt/handler.py
+++ nixt/handler.py
@@ -1,17 +1,19 @@
 # This file is placed in the Public Domain.
 
 
-"callback engine"
+"event handler"
 
 
 import queue
 import threading
+import time
+import _thread
 
 
-from .thread import launch
+from .runtime import launch
 
 
-class Engine:
+class Handler:
 
     def __init__(self):
         self.cbs = {}
@@ -59,7 +61,40 @@ class Engine:
         pass
 
 
+class Event:
+
+    def __init__(self):
+        self._ready = threading.Event()
+        self._thr = None
+        self.args = []
+        self.channel = ""
+        self.ctime = time.time()
+        self.rest = ""
+        self.result = {}
+        self.txt = ""
+        self.type = "event"
+
+    def done(self):
+        self.reply("ok")
+
+    def ready(self):
+        self._ready.set()
+
+    def reply(self, txt):
+        self.result[time.time()] = txt
+
+    def wait(self, timeout=None):
+        try:
+            self._ready.wait()
+            if self._thr:
+                self._thr.join()
+        except (KeyboardInterrupt, EOFError):
+            _thread.interrupt_main()
+
+
+
 def __dir__():
     return (
         'Engine',
-    )
+        'Event'
+   )
