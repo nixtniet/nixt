fa1d4d3bd28c5c2e1b11a6cfe85640d688748999
Bart Thate <bthate@dds.nl>
2025-09-08 05:50:14 +0200

    add table

---
+++ mods/tbl.py
@@ -0,0 +1,36 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "eml": "mbx",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "ls": "lst",
+    "mbx": "mbx",
+    "mre": "irc",
+    "nme": "rss",
+    "now": "mdl",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "udp": "udp",
+    "upt": "upt",
+    "wsd": "wsd"
+}
