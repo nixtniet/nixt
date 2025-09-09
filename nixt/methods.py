4272a04ffda0ee307ca888a908268646af7a69c8
Bart Thate <bthate@dds.nl>
2025-09-09 20:14:11 +0200

    over to test

--- nixt/methods.py
+++ nixt/methods.py
@@ -78,6 +78,65 @@ def name(obj):
     return ""
 
 
+def parse(obj, txt=None):
+    if txt is None:
+        if "txt" in dir(obj):
+            txt = obj.txt
+        else:
+            txt = ""
+    args = []
+    obj.args   = getattr(obj, "args", [])
+    obj.cmd    = getattr(obj, "cmd", "")
+    obj.gets   = getattr(obj, "gets", "")
+    obj.index  = getattr(obj, "index", None)
+    obj.mod    = getattr(obj, "mod", "")
+    obj.opts   = getattr(obj, "opts", "")
+    obj.result = getattr(obj, "result", "")
+    obj.sets   = getattr(obj, "sets", {})
+    obj.silent = getattr(obj, "silent", "")
+    obj.txt    = txt or getattr(obj, "txt", "")
+    obj.otxt   = obj.txt or getattr(obj, "otxt", "")
+    _nr = -1
+    for spli in obj.otxt.split():
+        if spli.startswith("-"):
+            try:
+                obj.index = int(spli[1:])
+            except ValueError:
+                obj.opts += spli[1:]
+            continue
+        if "-=" in spli:
+            key, value = spli.split("-=", maxsplit=1)
+            obj.silent[key] = value
+            obj.gets[key] = value
+            continue
+        if "==" in spli:
+            key, value = spli.split("==", maxsplit=1)
+            obj.gets[key] = value
+            continue
+        if "=" in spli:
+            key, value = spli.split("=", maxsplit=1)
+            if key == "mod":
+                if obj.mod:
+                    obj.mod += f",{value}"
+                else:
+                    obj.mod = value
+                continue
+            obj.sets[key] = value
+            continue
+        _nr += 1
+        if _nr == 0:
+            obj.cmd = spli
+            continue
+        args.append(spli)
+    if args:
+        obj.args = args
+        obj.txt  = obj.cmd or ""
+        obj.rest = " ".join(obj.args)
+        obj.txt  = obj.cmd + " " + obj.rest
+    else:
+        obj.txt = obj.cmd or ""
+
+
 def search(obj, selector, matching=False):
     res = False
     if not selector:
@@ -157,6 +216,7 @@ def __dir__():
         'fmt',
         'fqn',
         'name',
+        'parse',
         'search',
         'spl'
     )
