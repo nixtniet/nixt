a398381a26b38bacfec87e2c3282f8fe9cccf1c7
Bart Thate <bthate@dds.nl>
2025-09-18 23:05:21 +0200

    add caching and workdir modules

---
+++ nixt/caching.py
@@ -0,0 +1,154 @@
+# This file is placed in the Public Domain.
+
+
+"cache"
+
+
+import json.decoder
+import os
+import pathlib
+import threading
+import time
+
+
+from .methods import fqn
+from .objects import Object, items, update
+from .serials import dump, load
+from .workdir import getpath, long, store
+
+
+j = os.path.join
+lock = threading.RLock()
+
+
+class Cache:
+
+    objs = {}
+
+    @staticmethod
+    def add(path, obj):
+        Cache.objs[path] = obj
+
+    @staticmethod
+    def get(path):
+        return Cache.objs.get(path, None)
+
+    @staticmethod
+    def update(path, obj):
+        if not obj:
+            return
+        if path in Cache.objs:
+            update(Cache.objs[path], obj)
+        else:
+            Cache.add(path, obj)
+
+
+def cdir(path):
+    pth = pathlib.Path(path)
+    pth.parent.mkdir(parents=True, exist_ok=True)
+
+
+def find(clz, selector=None, deleted=False, matching=False):
+    clz = long(clz)
+    if selector is None:
+        selector = {}
+    for pth in fns(clz):
+        obj = Cache.get(pth)
+        if not obj:
+            obj = Object()
+            read(obj, pth)
+            Cache.add(pth, obj)
+        if not deleted and isdeleted(obj):
+            continue
+        if selector and not search(obj, selector, matching):
+            continue
+        yield pth, obj
+
+
+def fns(clz):
+    pth = store(clz)
+    for rootdir, dirs, _files in os.walk(pth, topdown=False):
+        for dname in dirs:
+            ddd = j(rootdir, dname)
+            for fll in os.listdir(ddd):
+                yield j(ddd, fll)
+
+
+def fntime(daystr):
+    datestr = " ".join(daystr.split(os.sep)[-2:])
+    datestr = datestr.replace("_", " ")
+    if "." in datestr:
+        datestr, rest = datestr.rsplit(".", 1)
+    else:
+        rest = ""
+    timed = time.mktime(time.strptime(datestr, "%Y-%m-%d %H:%M:%S"))
+    if rest:
+        timed += float("." + rest)
+    return float(timed)
+
+
+def isdeleted(obj):
+    return "__deleted__" in dir(obj) and obj.__deleted__
+
+
+def last(obj, selector=None):
+    if selector is None:
+        selector = {}
+    result = sorted(find(fqn(obj), selector), key=lambda x: fntime(x[0]))
+    res = ""
+    if result:
+        inp = result[-1]
+        update(obj, inp[-1])
+        res = inp[0]
+    return res
+
+
+def read(obj, path):
+    with lock:
+        with open(path, "r", encoding="utf-8") as fpt:
+            try:
+                update(obj, load(fpt))
+            except json.decoder.JSONDecodeError as ex:
+                ex.add_note(path)
+                raise ex
+
+
+def search(obj, selector, matching=False):
+    res = False
+    if not selector:
+        return res
+    for key, value in items(selector):
+        val = getattr(obj, key, None)
+        if not val:
+            continue
+        if matching and value == val:
+            res = True
+        elif str(value).lower() in str(val).lower() or value == "match":
+            res = True
+        else:
+            res = False
+            break
+    return res
+
+
+def write(obj, path=None):
+    with lock:
+        if path is None:
+            path = getpath(obj)
+        cdir(path)
+        with open(path, "w", encoding="utf-8") as fpt:
+            dump(obj, fpt, indent=4)
+        Cache.update(path, obj)
+        return path
+
+
+def __dir__():
+    return (
+        'Cache',
+        'cdir',
+        'find',
+        'fntime',
+        'last',
+        'read',
+        'write'
+    )
---
+++ nixt/workdir.py
@@ -0,0 +1,99 @@
+# This file is placed in the Public Domain.
+
+
+"persistence"
+
+
+import datetime
+import json.decoder
+import os
+import pathlib
+import threading
+import time
+
+
+j = os.path.join
+
+
+from .methods import fqn
+
+
+class Workdir:
+
+    name = __file__.rsplit(os.sep, maxsplit=2)[-2]
+    wdr  = os.path.expanduser(f"~/.{name}")
+
+
+def getpath(obj):
+    return store(ident(obj))
+
+
+def ident(obj):
+    return j(fqn(obj), *str(datetime.datetime.now()).split())
+
+
+def long(name):
+    split = name.split(".")[-1].lower()
+    res = name
+    for names in types():
+        if split == names.split(".")[-1].lower():
+            res = names
+            break
+    return res
+
+
+def moddir():
+    return j(Workdir.wdr, "mods")
+
+
+def pidname(name):
+    return j(Workdir.wdr, f"{name}.pid")
+
+
+def setwd(name, path=""):
+    path = path or os.path.expanduser(f"~/.{name}")
+    Workdir.wdr = path
+    skel()
+
+
+def skel():
+    result = ""
+    if not os.path.exists(store()):
+        pth = pathlib.Path(store())
+        pth.mkdir(parents=True, exist_ok=True)
+        pth = pathlib.Path(moddir())
+        pth.mkdir(parents=True, exist_ok=True)
+        result =  str(pth)
+    return result
+
+
+def store(pth=""):
+    return j(Workdir.wdr, "store", pth)
+
+
+def strip(pth, nmr=2):
+    return j(pth.split(os.sep)[-nmr:])
+
+
+def types():
+    skel()
+    return os.listdir(store())
+
+
+def wdr(pth):
+    return j(Workdir.wdr, pth)
+
+
+def __dir__():
+    return (
+        'Workdir',
+        'find',
+        'fntime',
+        'last'
+        'long',
+        'pidname',
+        'setwd',
+        'store',
+        'strip',
+        'types'
+    )
