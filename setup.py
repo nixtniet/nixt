5f74df55ebb1159eeaa5171031d7fb5ce24063a4
Bart Thate <bthate@dds.nl>
2025-12-16 04:05:19 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+
+"stub"
+
+
+import setuptools
+
+
+setuptools.setup(
+    data_files=[
+        ('share/nixt/examples', [
+            'examples/atr.py',
+            'examples/flt.py',
+            'examples/fnd.py',
+            'examples/irc.py',
+            'examples/log.py',
+            'examples/lst.py',
+            'examples/man.py',
+            'examples/mbx.py',
+            'examples/mdl.py',
+            'examples/mod.py',
+            'examples/pth.py',
+            'examples/req.py',
+            'examples/rss.py',
+            'examples/rst.py',
+            'examples/sil.py',
+            'examples/slg.py',
+            'examples/tdo.py',
+            'examples/thr.py',
+            'examples/tmr.py',
+            'examples/udp.py',
+            'examples/upt.py',
+            'examples/web.py',
+            'examples/wsd.py'
+            ]
+        )
+    ],
+    scripts=[
+        "bin/nixt",
+    ])
