42ba8e9c6525e300718e4589ac9f74efd6df0124
Bart Thate <nixtniet@gmail.com>
2026-01-09 23:01:16 +0100

    add modules

---
+++ nixt/modules/pth.py
@@ -0,0 +1,12 @@
+# This file is placed in the Public Domain.
+
+
+import os
+
+
+from nixt.utility import where
+
+
+def pth(event):
+    path = os.path.join(where(where), "nucleus", "index.html")
+    event.reply(f"file://{path}")
