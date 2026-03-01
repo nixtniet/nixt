116783fee6694f274cdda4a859b0f532520ac2b1
Bart Thate <nixtniet@gmail.com>
2026-03-01 02:07:08 +0100

    add methods tests

---
+++ testing/test_methods.py
@@ -0,0 +1,46 @@
+# This file is placed in the Public Domain.
+
+
+"methods"
+
+
+import unittest
+
+
+from nixt.methods import *
+
+
+class TestMethods(unittest.TestCase):
+
+    def test_deleted(self):
+        pass
+
+    def test_edit(self):
+        pass
+
+    def test_fmt(self):
+        pass
+
+    def test_fqn(self):
+        pass
+
+    def test_ident(self):
+        pass
+
+    def test_merge(self):
+        pass
+
+    def test_parse(self):
+        pass
+
+    def test_reduce(self):
+        pass
+
+    def test_search(self):
+        pass
+
+    def test_skip(self):
+        pass
+
+    def test_typed(self):
+        pass
