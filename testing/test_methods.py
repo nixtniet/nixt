1587677610bb59494dadaf8c25911d4b7bd03f2d
Bart Thate <nixtniet@gmail.com>
2026-03-01 02:32:35 +0100

    add methods tests

--- testing/test_methods.py
+++ testing/test_methods.py
@@ -8,39 +8,66 @@ import unittest
 
 
 from nixt.methods import *
+from nixt.objects import Object
 
 
 class TestMethods(unittest.TestCase):
 
     def test_deleted(self):
-        pass
+        obj = Object()
+        obj.__deleted__ = True
+        self.assertTrue(deleted(obj))
 
     def test_edit(self):
-        pass
+        obj = Object()
+        dct = {"a": "b"}
+        edit(obj, dct)
+        self.assertEqual(obj.a, "b")
 
     def test_fmt(self):
-        pass
+        obj = Object()
+        obj.a = "b"
+        self.assertTrue(fmt(obj), 'a="b"')
 
     def test_fqn(self):
-        pass
+        obj = Object()
+        self.assertTrue(fqn(obj), "nixt.objects.Object")
 
     def test_ident(self):
-        pass
+        obj = Object()
+        self.assertTrue("nixt.objects.Object" in ident(obj))
 
     def test_merge(self):
-        pass
+        obj = Object()
+        obj.a = "b"
+        merge(obj, {"a": ""})
+        self.assertEqual(obj.a, "b")
 
     def test_parse(self):
-        pass
+        obj = Object()
+        parse(obj, "cmd")
+        self.assertEqual(obj.cmd, "cmd")
 
     def test_reduce(self):
-        pass
+        obj = Object()
+        obj.a = "b"
+        obj.b = ""
+        dct = reduce(obj)
+        self.assertTrue("b" not in dct)
 
     def test_search(self):
-        pass
+        obj = Object()
+        obj.a = "b"
+        self.assertTrue(search(obj, {"a": "b"}))
 
     def test_skip(self):
-        pass
+        obj = Object()
+        obj.a = "b"
+        obj.b = "c"
+        dct = skip(obj, "b")
+        self.assertTrue("b" not in dct)
 
     def test_typed(self):
-        pass
+        obj = Object()
+        typed(obj, "a", "true")
+        self.assertTrue(obj.a)
