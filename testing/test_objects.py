4dca8ef9ea289df3b0833dcdb27a0046113571e1
Nixt Niet <nixtniet@gmail.com>
2026-03-08 18:51:45 +0100

    stop testing interfaces

--- testing/test_objects.py
+++ testing/test_objects.py
@@ -17,60 +17,8 @@ TARGET = nixt.objects
 VALIDJSON = '{"test": "bla"}'
 
 
-attrs1 = [
-    'Data',
-    'Dict',
-    'Methods',
-    'Object',
-]
-
-
-attrs2 = [
-    '__class__',
-    '__contains__',
-    '__delattr__',
-    '__dict__',
-    '__dir__',
-    '__doc__',
-    '__eq__',
-    '__firstlineno__',
-    '__format__',
-    '__ge__',
-    '__getattribute__',
-    '__getstate__',
-    '__gt__',
-    '__hash__',
-    '__init__',
-    '__init_subclass__',
-    '__iter__',
-    '__le__',
-    '__len__',
-    '__lt__',
-    '__module__',
-    '__ne__',
-    '__new__',
-    '__reduce__',
-    '__reduce_ex__',
-    '__repr__',
-    '__setattr__',
-    '__sizeof__',
-    '__static_attributes__',
-    '__str__',
-    '__subclasshook__',
-    '__weakref__'
-]
-
 
 class TestObject(unittest.TestCase):
-
-    def test_moduleinterface(self):
-        print(dir(TARGET))
-        self.assertTrue(dir(TARGET) == attrs1)
-
-    def test_objectinterface(self):
-        obj = Object()
-        print(dir(obj))
-        self.assertTrue(dir(obj) == attrs2)
 
     def test_constructor(self):
         obj = Object()
