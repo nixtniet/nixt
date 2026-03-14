a84496b4fcf5bb14d18979d4b228974948d3ece0
Nixt Niet <nixtniet@gmail.com>
2026-03-15 00:51:54 +0100

    update table

--- mods/tbl.py
+++ mods/tbl.py
@@ -1,3 +1,50 @@
+['tbl']
+['tbl']
+['fie']
+['fie', 'flt']
+['fie', 'flt', 'fnd']
+['fie', 'flt', 'fnd', 'irc']
+['fie', 'flt', 'fnd', 'irc', 'log']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr', 'tmr']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr', 'tmr', 'upt']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr', 'tmr', 'upt', 'wdr']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr', 'tmr', 'upt', 'wdr', 'web']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr', 'tmr', 'upt', 'wdr', 'web', 'wsd']
+['fie']
+['fie', 'flt']
+['fie', 'flt', 'fnd']
+['fie', 'flt', 'fnd', 'irc']
+['fie', 'flt', 'fnd', 'irc', 'log']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr', 'tmr']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr', 'tmr', 'udp']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr', 'tmr', 'udp', 'upt']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr', 'tmr', 'udp', 'upt', 'wdr']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr', 'tmr', 'udp', 'upt', 'wdr', 'web']
+['fie', 'flt', 'fnd', 'irc', 'log', 'man', 'mbx', 'mdl', 'pth', 'req', 'rss', 'rst', 'sil', 'slg', 'tbl', 'tdo', 'thr', 'tmr', 'udp', 'upt', 'wdr', 'web', 'wsd']
 # This file is placed in the Pubic Domain.
 
 
@@ -38,27 +85,5 @@ NAMES = {
 
 
 MD5 = {
-    "mods.tbl": "d41d8cd98f00b204e9800998ecf8427e",
-    "mods.fie": "8764d7a9613a9018ec273ce8f3f800e6",
-    "mods.flt": "2ff87dac41c4f1afa50a6f0014e9e35c",
-    "mods.fnd": "a7442773cf6e5c9387486c42f9e34f23",
-    "mods.irc": "49b7b8ba122f4af2112fe3e7d60fb2c7",
-    "mods.log": "9939174052e70c618eeb833c05a08b4f",
-    "mods.man": "5d2d5d375028c82831c024114e605f1b",
-    "mods.mbx": "302172749a1c34eaf96410eb8d8a09e3",
-    "mods.mdl": "ac569bad2f3358975edd221d81aabf60",
-    "mods.pth": "0757fe9e50b98c6e3a131693ad252288",
-    "mods.req": "bc1984d2e9de0310dc1b468f25c7ab8c",
-    "mods.rss": "11dff8744ec6ead019f50247e464a40f",
-    "mods.rst": "baea77897e6ccdb1137db0019b161913",
-    "mods.sil": "fcceb6f048dd756ae9e2bad8c9793ad8",
-    "mods.slg": "0196cecb8ff67e032bd389a0b1860353",
-    "mods.tdo": "9223232a5a50ae732803898641b57ddc",
-    "mods.thr": "942bc0354e26935adc97065feb50e9da",
-    "mods.tmr": "4cc3aaf3d07dc69f1611b3bab0b831d0",
-    "mods.upt": "1af6772bff48f035752869e45799ca40",
-    "mods.wdr": "9bc2f166297b19a5fb964b500f5d91af",
-    "mods.web": "f77dfb972af0d1ef3442f56b60c4ba2a",
-    "mods.wsd": "1ddb0329e6cfbaf6349a1720e2d25a0c",
-    "mods.udp": "ce48fc590c291a7eb3ae546d75bb70d3"
+    "modules.udp": "ce48fc590c291a7eb3ae546d75bb70d3"
 }
