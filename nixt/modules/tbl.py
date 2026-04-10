73db2a1166a246512854c8cd818389286e89ba3b
Nixt Niet <nixtniet@gmail.com>
2026-04-10 22:27:22 +0200

    add table

---
+++ nixt/modules/tbl.py
@@ -0,0 +1,26 @@
+# This file is placed in the Pubic Domain.
+
+
+"tables"
+
+
+NAMES = {
+    "cmd": "adm",
+    "mod": "adm",
+    "tbl": "adm",
+    "ver": "adm",
+    "wdr": "wdr",
+    "cfg": "cfg",
+    "fie": "fie",
+    "fnd": "fnd"
+}
+
+
+MD5 = {
+    "nixt.modules.adm": "0fab28a8309102856ffd10b0b428fd65",
+    "nixt.modules.cfg": "09d8a42e68ca9cea75ac9b599c4e603b",
+    "nixt.modules.fie": "8764d7a9613a9018ec273ce8f3f800e6",
+    "nixt.modules.fnd": "4343f26d7060f679c5af9a919d4cfd7d",
+    "nixt.modules.tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "nixt.modules.wdr": "9bc2f166297b19a5fb964b500f5d91af"
+}
