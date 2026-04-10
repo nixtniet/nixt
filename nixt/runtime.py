62c7599c62b05b3e5cfd3e9873a1a68a460c11a3
Nixt Niet <nixtniet@gmail.com>
2026-04-10 02:11:21 +0200

    uhhhh

--- nixt/runtime.py
+++ nixt/runtime.py
@@ -10,6 +10,7 @@ import sys
 import time
 
 
+from .caching import Workdir
 from .command import Commands, Event, Main, Mods
 from .handler import Client
 from .objects import Methods
@@ -31,19 +32,17 @@ class Line(Client):
 
 class Run:
 
-    @classmethod
-    def banner(cls):
-        "hello."
-        tme = time.ctime(time.time()).replace("  ", " ")
-        print("%s %s since %s %s (%s)" % (
-            Main.name.upper(),
-            Main.version,
-            tme,
-            Main.level.upper() or "INFO",
-            Utils.md5sum(Mods.path("tbl") or "")[:7]
-        ))
-        sys.stdout.flush()
-        return Main.version
+    @staticmethod
+    def boot():
+        Main.wdr = os.path.expanduser(f"~/.{Main.name}")
+        Methods.parse(Main, TXT)
+        Methods.merge(Main, Main.sets)
+        Workdir.skel()
+        if Run.check("u"):
+            Mods.add("mods")
+        Mods.add(os.path.join(Main.wdr, "mods"), "modules")
+        Mods.pkg(MODS)
+        Commands.scanner()
 
     @staticmethod
     def check(opts):
@@ -55,45 +54,16 @@ class Run:
                     return True
         return False
 
-    @staticmethod
-    def cmd(text):
-        "parse text for command and run it."
-        cli = Line()
-        for txt in TXT.split(" ! "):
-            evt = Event()
-            evt.client = cli
-            evt.orig = repr(cli)
-            evt.text = txt
-            Commands.command(evt)
-        return evt
-
-    @classmethod
-    def wrap(cls, func, *args):
-        "restore console."
-        import termios
-        old = None
-        try:
-            old = termios.tcgetattr(sys.stdin.fileno())
-        except termios.error:
-            pass
-        try:
-            func(*args)
-        except (KeyboardInterrupt, EOFError):
-            pass
-        except Exception as ex:
-            logging.exception(ex)
-        if old:
-            termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, old)
-
 
 def main():
-    Main.wdr = os.path.expanduser(f"~/.{Main.name}")
-    Mods.add("mods")
-    Mods.pkg(MODS)
-    Methods.parse(Main, TXT)
-    Commands.scanner()
-    Run.cmd(TXT)
+    Run.boot()
+    csl = Line()
+    for txt in TXT.split(" ! "):
+        evt = Event()
+        evt.client = csl
+        evt.text = txt
+        Commands.command(evt)
 
 
 if __name__ == "__main__":
-    Run.wrap(main)
+    main()
