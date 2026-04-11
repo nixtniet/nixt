a281c59b86a9ca663689a82ad3c3709a018f1119
Nixt Niet <nixtniet@gmail.com>
2026-04-12 00:09:14 +0200

    update table

--- nixt/modules/tbl.py
+++ nixt/modules/tbl.py
@@ -5,12 +5,18 @@
 
 
 CORE = {
+    "configs": "c8cc832e1d893e5ae59811cf2179fc18",
     "objects": "16b65ec81fc90afe874d51fcd3b9c05e",
-    "command": "1673cda53279f7a8f1601fa79bd599b3",
-    "utility": "0292fe44bb971931651845f397aed056",
-    "persist": "7312bb12c92c27d6ddca5d14a8076cd8",
-    "handler": "9e14098e664bb72a3393a03551a548ad",
-    "booting": "f4d8f5c112db2dedf31e5f0acc20250b"
+    "command": "88e1e39088fdd4ade5a34bf5e4eb9954",
+    "package": "5f16a314e1be4d76e92bd2cb244c4bc9",
+    "loggers": "07886850aee355b0ce2fb172ff3acb04",
+    "utility": "c489a0d2952ec10cd6ccebef5a38448e",
+    "persist": "ffec3d8a4c261fe223127bdc88838146",
+    "handler": "4ec54eaef5829351efa8bc7f43498798",
+    "threads": "b22b7875ae00039baf62a42e5ce1c25d",
+    "booting": "b05db78db7ea9887e134ba909b965964",
+    "clients": "327a41589fd0738aac32be0f43a3bf9d",
+    "runtime": "42257d4ccb4440e958840f9a8c0849e0"
 }
 
 
@@ -27,8 +33,8 @@ NAMES = {
 
 
 MD5 = {
-    "nixt.modules.adm": "9cd0f967c9a0aff9d7268922cf82df8b",
-    "nixt.modules.cfg": "09d8a42e68ca9cea75ac9b599c4e603b",
+    "nixt.modules.adm": "2cdc3c9b2956ef7b11bfe7738ee4e69b",
+    "nixt.modules.cfg": "c115363707550d407000dbfcdf9a587c",
     "nixt.modules.fie": "8764d7a9613a9018ec273ce8f3f800e6",
     "nixt.modules.fnd": "4343f26d7060f679c5af9a919d4cfd7d",
     "nixt.modules.tbl": "d41d8cd98f00b204e9800998ecf8427e",
