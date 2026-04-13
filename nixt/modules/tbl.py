6b5bab6dc46d9299765bb9b8ebf5e3709d566232
Nixt Niet <nixtniet@gmail.com>
2026-04-14 00:06:26 +0200

    move fnd to mods

---
+++ nixt/modules/tbl.py
@@ -0,0 +1,39 @@
+# This file is placed in the Pubic Domain.
+
+
+"tables"
+
+
+CORE = {
+    "configs": "89402595885fd5e851ae221b89e067bc",
+    "objects": "e020de90536d3537d635d66d02958c1d",
+    "command": "02299bfadda7878a47b7a0558249e468",
+    "package": "1a8d5525b6ac49990765b5b2dbb20cd7",
+    "utility": "f5cbfe669c8f3dad9015d3d3ae44f22f",
+    "brokers": "0e4c27ffe2ed97146dd7a346cae1734c",
+    "persist": "0b6480f1e7973f6974b717a4d06a4170",
+    "encoder": "93b6347168b807e7dc589e591831ce04",
+    "handler": "693301eb4936b560247f55f1ae338d6e",
+    "threads": "c8dc6421e843dd0632740976b1649fb2",
+    "booting": "9e7ebcbdb5014ab07575f83b268b63df",
+    "runtime": "91a804f49a949da41d06ddad38d70419"
+}
+
+
+NAMES = {
+    "cmd": "adm",
+    "mod": "adm",
+    "ver": "adm",
+    "cfg": "cfg",
+    "fie": "fnd",
+    "fnd": "fnd",
+    "tbl": "runtime"
+}
+
+
+MD5 = {
+    "modules.adm": "1894a1bd0f9e9e06cefda4d6a886f177",
+    "modules.cfg": "1ad73a274fa5ccf5150215e2d882325a",
+    "modules.fnd": "2d3aed0fc5476fb422f97b8d91d08461",
+    "modules.tbl": "d41d8cd98f00b204e9800998ecf8427e"
+}
