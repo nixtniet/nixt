a0e35c7648fcbdd2a22f8c9f9bc7f2849a28f811
Nixt Niet <nixtniet@gmail.com>
2026-04-17 03:26:46 +0200

    update table

---
+++ mods/tbl.py
@@ -0,0 +1,111 @@
+# This file is placed in the Pubic Domain.
+
+
+"tables"
+
+
+CORE = {
+    "configs": "79fd61ebe02f2c7a8d550664faf7c98b",
+    "objects": "744383d02a432cd133d59d20c095e403",
+    "command": "b7dddb61ca85797909eaf6088422a23b",
+    "package": "4348d78f33a2df4a79a5794daa9e5332",
+    "utility": "19cdaaad2d4c604cf9c4e21c63bf0739",
+    "brokers": "0e4c27ffe2ed97146dd7a346cae1734c",
+    "persist": "e9ba353b520413155eb4b2f4e12a52fc",
+    "encoder": "99a5714698346a23e399605fb98c3efe",
+    "handler": "5c6c018b23f44b74bd9acbb19095bc4f",
+    "threads": "c8dc6421e843dd0632740976b1649fb2",
+    "booting": "510e6053c710b2b45afadc242f141481",
+    "runtime": "aea146dc1476075f2b07b7a97080244a"
+}
+
+
+NAMES = {
+    "cmd": "adm",
+    "mod": "adm",
+    "ver": "adm",
+    "wdr": "wdr",
+    "cfg": "cfg",
+    "krn": "cfg",
+    "fie": "fnd",
+    "flt": "flt",
+    "fnd": "fnd",
+    "hlp": "hlp",
+    "log": "log",
+    "man": "man",
+    "eml": "mbx",
+    "mbx": "mbx",
+    "dis": "mdl",
+    "now": "mdl",
+    "pth": "pth",
+    "pwd": "pwd",
+    "req": "req",
+    "atr": "rss",
+    "dpl": "rss",
+    "err": "rss",
+    "exp": "rss",
+    "imp": "rss",
+    "nme": "rss",
+    "rem": "rss",
+    "res": "rss",
+    "rss": "rss",
+    "syn": "rss",
+    "lou": "sil",
+    "sil": "sil",
+    "slg": "slg",
+    "dne": "tdo",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "udp": "udp",
+    "upt": "upt",
+    "wsd": "wsd"
+}
+
+
+MD5 = {
+    "mods.adm": "3c85c99095248d55284398d0780960ea",
+    "mods.cfg": "da4f2344c14fc45aa347adf927cda3b6",
+    "mods.fie": "6c38143e012c48ee08af29c2e35e8898",
+    "mods.flt": "68b06f967b47b2567c8eccb7113a72f0",
+    "mods.fnd": "2d3aed0fc5476fb422f97b8d91d08461",
+    "mods.hlp": "3739c32bae9a8a97224fae142cca9067",
+    "mods.irc": "f758cb6a93e8cfa072a3196c8078c817",
+    "mods.log": "30ba4f3af77a90b4915825c84c70573d",
+    "mods.man": "9b1ee43a507a6f4dbccadb8658611cd4",
+    "mods.mbx": "e89cfeb3faef185edc569c215e5ed0a3",
+    "mods.mdl": "d1cfa7c33cde8275e1f254de833dfa6b",
+    "mods.pth": "211bf9ef6898555135249123b584fe6c",
+    "mods.pwd": "c308a6405f500bbcdde84334fc377bb8",
+    "mods.req": "272635755a01b6e21d39ffba6a219e43",
+    "mods.rss": "8092df92d7bec8739967a65b6a3ef6bd",
+    "mods.rst": "b37e4cee4a60b8a09ab7d5168bd14782",
+    "mods.sil": "fcceb6f048dd756ae9e2bad8c9793ad8",
+    "mods.slg": "0196cecb8ff67e032bd389a0b1860353",
+    "mods.tdo": "2e2b728c248339487e483755e3669a1a",
+    "mods.thr": "942bc0354e26935adc97065feb50e9da",
+    "mods.tmr": "3c5d55ef7f2397844b4eb083a60c0def",
+    "mods.udp": "ffc992e8138511a865b8c0881d9799d4",
+    "mods.upt": "1af6772bff48f035752869e45799ca40",
+    "mods.wdr": "9bc2f166297b19a5fb964b500f5d91af",
+    "mods.web": "a1a9d1506a5a627357342b149522d4f4",
+    "mods.wsd": "1ddb0329e6cfbaf6349a1720e2d25a0c"
+}
+
+
+SKIPS = {
+    "wdr": "irc",
+    "cfg": "irc",
+    "krn": "irc",
+    "fnd": "irc",
+    "hlp": "irc",
+    "man": "irc",
+    "eml": "irc",
+    "mbx": "irc",
+    "req": "irc",
+    "err": "irc",
+    "exp": "irc",
+    "imp": "irc",
+    "syn": "irc",
+    "udp": "irc"
+}
