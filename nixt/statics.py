3269643f0d13ec7d7ef6dad78117ddcccd54fddd
Nixt Niet <nixtniet@gmail.com>
2026-05-06 04:14:24 +0200

    add statics

---
+++ nixt/statics.py
@@ -0,0 +1,47 @@
+# This file is placed in the Public Domain.
+
+
+"static files"
+
+
+NAMES = {
+    "atr": "rss",
+    "cfg": "cfg",
+    "cmd": "bsc",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "eml": "mbx",
+    "err": "rss",
+    "exp": "rss",
+    "fie": "fnd",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "lou": "sil",
+    "man": "man",
+    "mbx": "mbx",
+    "mod": "bsc",
+    "nme": "rss",
+    "now": "mdl",
+    "pth": "pth",
+    "pwd": "pwd",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "sil": "sil",
+    "slg": "slg",
+    "srv": "adm",
+    "syn": "rss",
+    "tbl": "adm",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "udp": "udp",
+    "upt": "bsc",
+    "ver": "bsc",
+    "wdr": "adm",
+    "wsd": "wsd"
+}
