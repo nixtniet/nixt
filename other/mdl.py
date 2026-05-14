6251e445e49167b31ef51823c3421f09df489140
Nixt Niet <nixtniet@gmail.com>
2026-05-14 02:32:36 +0200

    linters

--- other/mdl.py
+++ other/mdl.py
@@ -9,7 +9,7 @@ import logging
 import time
 
 
-from nixt.defines import Base, Broker, Message, Object, Object, Repeater, Time
+from nixt.defines import Base, Broker, Message, Object, Repeater, Time
 
 
 def init():
