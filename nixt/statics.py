000aa4679ca682654166e3eadbbe603b3c3445ba
Nixt Niet <nixtniet@gmail.com>
2026-05-28 01:52:03 +0200

    update package

---
+++ nixt/statics.py
@@ -0,0 +1,96 @@
+# This file is placed in the Public Domain.
+
+
+"static tables"
+
+
+CORE = {
+    "brokers": "ba7f144f1598b37bdfda200070cc9298",
+    "clients": "b104551450cd38ff046a8520a28fbd59",
+    "command": "a433a99d7d7e29c33eb03d73a90723e7",
+    "configs": "e0ecb62291da42ed7a5c80d1738eb9ce",
+    "defines": "0c2f6c7a779eeb1b2bc48df80e2bebf2",
+    "engines": "3b7d6903c4739c3cf1b56da2a720144d",
+    "handler": "13c6344d772b3cbc260e2796c606c5f0",
+    "loggers": "326a1c789f4d75c09ceeb59e2b625d4c",
+    "message": "13c6344d772b3cbc260e2796c606c5f0",
+    "objects": "59dc464a02723f25630361545ac6eaa8",
+    "parsers": "5e27b3bb8b3e2026a97d08dd3ac0cff8",
+    "persist": "6dc1c42ac395f0533cfc85b1f3914534",
+    "repeats": "7c8492ab2d848c15c780c3bfde8bd8ce",
+    "runtime": "2a6c39a2483d2bd34ba895bf5466a544",
+    "threads": "43527fea635612e61d00c764d9a01310",
+    "timings": "326a1c789f4d75c09ceeb59e2b625d4c",
+    "utility": "326a1c789f4d75c09ceeb59e2b625d4c"
+}
+
+
+MODULES = {
+    "adm": "2ee355f9b20e223e1471a0d34050e474",
+    "bsc": "36247da5127f2c417b87b08a23dfed77",
+    "cfg": "81df231cf8b34e632da15da1f9f34638",
+    "flt": "2d22327b1aea5cb5cff50850a4a2452e",
+    "fnd": "2b45397414f2bacb86845e987005ddcf",
+    "irc": "f4ea7c566fc68f3b15217e79bb890075",
+    "log": "5c5ece44e481e26e0bb249262ca43dde",
+    "man": "285170123721322dcd19ec7d69f84ee9",
+    "mbx": "609e9241f0d91b95833be5209ee75da3",
+    "mdl": "e31a848b91836189775f5fdaa1e7fed9",
+    "pth": "653000ba52bd13a2c6690f7049a4c5d3",
+    "pwd": "3ce569eae80824787de0c2c5b7874ead",
+    "req": "3463c19a588084cc8208122e46ac144e",
+    "rss": "76cf239f5e20386cc32dddb747365bee",
+    "rst": "becddec4f9088f9c927224cd7e85ab42",
+    "sil": "7de0007925c823be72ec70305581e861",
+    "slg": "e68f11973ddc2e3edeb0de0e16e9fe7a",
+    "tdo": "69496e541a953287b3114ac7b9260d56",
+    "thr": "da08208acac4f293332f92bb3a961f88",
+    "tmr": "2182ea8cf5a55374fb135bce7f354c22",
+    "udp": "1d4f8e36b4d502b18b5fdf35f81f0693",
+    "web": "cda5b00b4fc79c242287fed32014277f",
+    "wsd": "f9056c98589ff12ee465b9c0c1b324b2"
+}
+
+
+NAMES = {
+    "atr": "rss",
+    "cfg": "cfg",
+    "cmd": "bsc",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "eml": "mbx",
+    "err": "rss",
+    "exp": "rss",
+    "fie": "fnd",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "lou": "sil",
+    "man": "man",
+    "mbx": "mbx",
+    "mod": "bsc",
+    "nme": "rss",
+    "now": "mdl",
+    "pth": "pth",
+    "pwd": "pwd",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "sil": "sil",
+    "skl": "adm",
+    "slg": "slg",
+    "srv": "adm",
+    "syn": "rss",
+    "tbl": "adm",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "udp": "udp",
+    "upt": "bsc",
+    "ver": "bsc",
+    "wdr": "adm",
+    "wsd": "wsd"
+}
