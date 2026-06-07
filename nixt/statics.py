b8d33571e76dd5fa78645fe2d0f33521400ff8a9
Nixt Niet <nixtniet@gmail.com>
2026-06-07 02:01:01 +0200

    update statics

---
+++ nixt/statics.py
@@ -0,0 +1,89 @@
+# This file is placed in the Public Domain.
+
+
+"static tables"
+
+
+COMPLETIONS = [
+    "admin.service",
+    "admin.table",
+    "config.edit",
+    "email.read",
+    "email.show",
+    "irc.pwd",
+    "log.add",
+    "manual.create",
+    "model.disease",
+    "model.now",
+    "path.path",
+    "request.show",
+    "rss.add",
+    "rss.attributes",
+    "rss.display",
+    "rss.errors",
+    "rss.export",
+    "rss.opml",
+    "rss.name",
+    "rss.remove",
+    "rss.restore",
+    "rss.sync",
+    "show.cmd",
+    "show.fields",
+    "show.fleet",
+    "show.objects",
+    "show.threads",
+    "show.uptime",
+    "show.version",
+    "silent.off",
+    "silent.on",
+    "slogan.show",
+    "timer.add",
+    "todo.add",
+    "todo.done",
+    "udp.send",
+    "wisdom.show"
+]
+
+
+CORE = {
+    "booting": "800ddace37bd2dfc56cf0530cb58dc6d",
+    "brokers": "b0f2374e516c4ba8581b728b7d3cc026",
+    "clients": "0de88d3b063319b47418b62710d92e0c",
+    "command": "3ee136bd134ed5e8b4f07534f729d9bd",
+    "configs": "b054626f8d5171e35cb84ba02bfe4347",
+    "defines": "c635aa475d02993b770a9384ce9a290e",
+    "engines": "3b7d6903c4739c3cf1b56da2a720144d",
+    "handler": "99191b6e52700721de004ea446f46388",
+    "message": "6cdc4fe6ea706c60ac600076faea9706",
+    "objects": "742237af0e0ff8f09d08b58e0a547e80",
+    "package": "45e86d174e71537d0d4e6ec118d20910",
+    "parsers": "8af0b703167b66dd234a3293d06ff598",
+    "persist": "7200ecae797892694939b78c29600623",
+    "repeats": "b3e2f394fb569e31cf6cf160b5731280",
+    "runtime": "ed941e3697f861b0cd3a175f29fcd991",
+    "threads": "3fa17f6805fef5256605ae9ed9bd4a72",
+    "utility": "e3b1dfbd4650bed381f91f2d27a6d115"
+}
+
+
+MODULES = {
+    "admin": "eb7e4734db5f36f9af842df3730c9e87",
+    "config": "a60039e8fddb8bfc5579b9db29507fd8",
+    "email": "ff8a28b6823e29bfdd7f58fa3c2e54f2",
+    "irc": "6456717e6590b0907d79dee95791c500",
+    "log": "f17670435f3980bb9dcac701ae50802c",
+    "manual": "1e03b1574d2e24d6cc41940a2212fc5f",
+    "model": "310dc1f3246513fdf729a13f9fd8d4b4",
+    "path": "1dbb1d2ede1fdd9ec920604cb8fd6689",
+    "request": "f0610d879a8d1242b5d247f520870155",
+    "rest": "b6798332298ddc8a097a2f6c343462cd",
+    "rss": "b511e336ef916cf8b507bb0038db5188",
+    "show": "bb188cf51c7c76ecfbb1d4760e28e0cd",
+    "silent": "9309b68eb72540982700136710ed12a1",
+    "slogan": "16c704f6c30cee642183ff0cdafe96d3",
+    "timer": "42ed812ad2deb8400d5ab3095df4c13b",
+    "todo": "3991419714a5ac0a8561f0e88047ec3a",
+    "udp": "e5b0449dfa47e29a0557d7d643aa08e2",
+    "web": "cda5b00b4fc79c242287fed32014277f",
+    "wisdom": "f9778e20ace4b3b3c66ee44367057606"
+}
