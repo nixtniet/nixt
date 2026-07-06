a2827e6c3f54d024eaf7870b72f062fb30a563a1
Nixt Niet <nixtniet@gmail.com>
2026-07-06 21:06:20 +0200

    update tables

--- nixt/statics.py
+++ nixt/statics.py
@@ -5,18 +5,18 @@
 
 
 CORE = {
-    "booting": "afa460aaeca3dcbdc24fae65748d5a2d",
+    "booting": "d14b171dd49a9bedac0b3a1c6146a58b",
     "brokers": "ba7f144f1598b37bdfda200070cc9298",
     "clients": "1a5cb5478cf36ab9a5a4568df3c338eb",
-    "configs": "e75d136d5d124be46e305e108af67f9c",
+    "configs": "c1e2f18dbcf11d4e475f608bb04367f6",
     "defines": "c97d6a0b371829d71a1ca39c09b2e279",
     "encoder": "442b637a07a3611696a06d1f2c8211f5",
     "engines": "f9950d5c51f1382ebb251fbadcb1de9f",
     "loggers": "0c75c9b5df26dc5f023257711f140754",
     "message": "ab5a0b4160b5f8f6d9a8dedb9ed7714c",
-    "objects": "5a6b2a4692c40e17c593a8a39a8743e3",
-    "package": "361e212b3601ed80ef6ad2e9c01e9048",
-    "parsers": "11b60a600984f3420d6589da98f68ecf",
+    "objects": "8ff9c08a27bb5134051745d98916cf6e",
+    "package": "1a9573bfa408134735206890a67c0a1a",
+    "parsers": "62e7d19410c0fdb74b101be730ab82a6",
     "persist": "5b0efac784bf62e8eceaea8287bd2db1",
     "repeats": "5f6ee691ff3dd90df68d34805b229298",
     "runtime": "f1193898f5c9d42769627598c092e4b8",
