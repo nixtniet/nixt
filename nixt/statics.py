5f71de423a46c6a1dd46f0d8262a089c321c0a88
Nixt Niet <nixtniet@gmail.com>
2026-07-17 02:43:10 +0200

    update statics

--- nixt/statics.py
+++ nixt/statics.py
@@ -7,9 +7,8 @@
 CORE = {
     "message": "e8732b08af0edccbbae0543f445cfa74",
     "objects": "67a1bb711018d43af33c49cb15792c26",
-    "package": "dc5c6b51e3db87dc6d2d0821efbad3ba",
+    "package": "ba6ae6ce2a726410aae28669d78d2e34",
     "persist": "fb0779b501b02464badf10dc82173346",
-    "runtime": "2d98bbf7a693c30cda3f00416e90b4ea",
     "utility": "4fd6534e081de6ebfe23d209531b8d81"
 }
 
