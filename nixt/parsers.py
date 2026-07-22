6888566fa080660d88406481434b637490d09570
Nixt Niet <nixtniet@gmail.com>
2026-07-22 02:46:10 +0200

    add missing modules

---
+++ nixt/parsers.py
@@ -0,0 +1,76 @@
+# This file is placed in the Public Domain.
+
+
+"command parsing"
+
+
+from .objects import Default, Method
+
+
+class Parse:
+
+    @staticmethod
+    def parse(obj, text, clean=False):
+        "parse text for command and arguments."
+        data = {
+            "args": [],
+            "cmd": "",
+            "gets": Default(),
+            "index": None,
+            "init": "",
+            "mod": "",
+            "opts": "",
+            "otxt": text,
+            "rest": "",
+            "silent": Default(),
+            "sets": Default(),
+            "text": text
+        }
+        for k, v in data.items():
+            if not clean:
+                setattr(obj, k, getattr(obj, k, v) or v)
+            else:
+                setattr(obj, k, v)
+        args = []
+        nr = -1
+        for spli in text.split():
+            if spli.startswith("-"):
+                try:
+                    obj.index = int(spli[1:])
+                except ValueError:
+                    obj.opts += spli[1:]
+                continue
+            if "-=" in spli:
+                key, value = spli.split("-=", maxsplit=1)
+                Method.typed(obj.silent, key, value)
+                Method.typed(obj.gets, key, value)
+                continue
+            if "==" in spli:
+                key, value = spli.split("==", maxsplit=1)
+                Method.typed(obj.gets, key, value)
+                continue
+            if "=" in spli:
+                key, value = spli.split("=", maxsplit=1)
+                Method.typed(obj.sets, key, value)
+                continue
+            nr += 1
+            if nr == 0:
+                try:
+                    obj.mod, obj.cmd = spli.split(".")
+                except ValueError:
+                    obj.cmd = spli
+                continue
+            args.append(spli)
+        if args:
+            obj.args = args
+            obj.text = obj.mod + " " + obj.cmd
+            obj.rest = " ".join(obj.args)
+            obj.text = obj.text + " " + obj.rest
+        else:
+            obj.text = obj.mod + " " + obj.cmd
+
+
+def __dir__():
+    return (
+        'Parse',
+    )
