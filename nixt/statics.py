e35ce74aa6f1be3277d5ec7b4780e8b5f637a20f
Nixt Niet <nixtniet@gmail.com>
2026-07-25 00:33:01 +0200

    update statics

--- nixt/statics.py
+++ nixt/statics.py
@@ -21,34 +21,34 @@ CORE = {
     "repeats": "1d3ead9599a5ebd6aa58344ba9bd8218",
     "runtime": "67c0c41b5ebf7d86e3802d682202c5fb",
     "threads": "2fcb5ceb0fa336dd7208297fc23e17b0",
-    "utility": "991da4d4ae2caed0f025639bc2789d80"
+    "utility": "2679b527c0528dafda4ab730cd5c5e60"
 }
 
 
 MODULES = {
-    "cfg": "b3f539bc2fb89e0fdc40de95c4b1e704",
-    "eml": "780c7efa9e2e2b94b2edad1bcf6a5eb8",
-    "fie": "4f7469f16134dfcd6b124bd2f28d4760",
-    "flt": "1fa811bc0ab9cb5b5b6613ca046f9c3f",
-    "fnd": "6c3f753fa543e2a1489ce58034d6a779",
-    "irc": "2f036fdd352200c5369cdbabe029d145",
-    "log": "7e9156a8fd67fc01f1f8240773614637",
+    "cfg": "7fd2407f70f938d99fc797064b99a87e",
+    "eml": "f109afc8b6c5d46706a737ff94e1756a",
+    "fie": "e0612b0410a5530381ca49692a74b091",
+    "flt": "c40a68583139d18206d885959c0ece30",
+    "fnd": "103ad8d13cdafc229b1b1e5e099951db",
+    "irc": "b361661bc8d3fb9e21565bba743400f5",
+    "log": "078aea11abb982ff947b74a016954902",
     "man": "920599410f7739c9503e0eea9e4e5885",
-    "mdl": "a894ae3d8d6ddc6573f3c1b1290dda74",
+    "mdl": "0ea87138ce166ce9a904514ab8bc4b48",
     "pth": "de5a301a26d1cc548fb53ac4aa5550db",
     "req": "bc1984d2e9de0310dc1b468f25c7ab8c",
-    "rss": "b730499fb662ca0fade82d96e6b4e176",
-    "rst": "524608311cb71e686ab34b65adaf4d17",
-    "sil": "1b175334e5a9a006c7b58ae8e9a0f0c9",
+    "rss": "829eb4893838d2a48ffd19b6295cbf37",
+    "rst": "39b1e1c5fe013f6a19aa3d378a383cf7",
+    "sil": "5ff461fdc5505facd8efcf38e51a725f",
     "slg": "e68f11973ddc2e3edeb0de0e16e9fe7a",
-    "tdo": "3edcb747b87fa1fe96412df51e0f24ae",
-    "thr": "a59544c0c0026efd975cf75e210caeb6",
-    "tmr": "74df1496d1c3eec1b65b3641a2456750",
-    "udp": "e9a4f41e0f29382335ff6d3d57f980ec",
-    "upt": "847a09522abd97a7799a5f5474182064",
-    "ver": "e43d64ec467f26dcb0549eaa4d7f5794",
-    "web": "75583a59839bf45be5e6b8c68cb01551",
-    "wsd": "f160d4246e688fba633ae35bd45c7788"
+    "tdo": "845479d136166e6e4cd6c7d6d9e04bec",
+    "thr": "6ae37c096264d246cb4de07cfd687222",
+    "tmr": "f3d6df1887dae91a3023350749f4217a",
+    "udp": "dceb7ac06569d8ac001c1c320db8f9e1",
+    "upt": "d7f456e017f217289720a0ddda3aa24d",
+    "ver": "be405e2a3d958c3ae704adb3d3fb3c6d",
+    "web": "918b093ec022c3ab17a4773963415c07",
+    "wsd": "a9d4f77c24929ec37b62044b9e83023a"
 }
 
 
