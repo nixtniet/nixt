1afc5171fe3a7d475a4c61ce3c9bf339c2a98a0f
Nixt Niet <nixtniet@gmail.com>
2026-08-07 19:35:43 +0200

    add statics

---
+++ nixt/statics.py
@@ -0,0 +1,38 @@
+# This file is placed in the Public Domain.
+
+
+"static tables"
+
+
+CORE = {
+    "brokers": "bf614fd92d3268216c853bbb08a57b03",
+    "clients": "04e78280804354fe91541e6c6e4d7bf0",
+    "configs": "5c8bc5f93e17340031ca8b9be01572de",
+    "defines": "5a05478df73bfbbe25a2f4f44fc223e0",
+    "engines": "c98dd8077ceb5ef4b10df3e30951a97b",
+    "methods": "76074300b72a45dee8d20ef76aa410ed",
+    "objects": "86b923dd874fd9781a1e46db0d35340d",
+    "package": "7ef9d27ac2f9c6e44a62d99f05d74a6c",
+    "persist": "2be083d8f6cb0ce16f1bc31224ad0593",
+    "threads": "2fcb5ceb0fa336dd7208297fc23e17b0",
+    "utility": "252e94e8db1c13b1bbba8beeb0f5529e"
+}
+
+
+MODULES = {
+    "cfg": "83bd7e9b313fb55fc46c7bf797a70f77",
+    "fnd": "de16eca3ee25ffd940562b3d192081b2",
+    "log": "5d11a098f0c298fe773f8d9bfbb21d11",
+    "tdo": "734baf117bde28e7a979bdec82528c14",
+    "ver": "be405e2a3d958c3ae704adb3d3fb3c6d"
+}
+
+
+NAMES = {
+    "cfg": "cfg",
+    "dne": "tdo",
+    "fnd": "fnd",
+    "log": "log",
+    "tdo": "tdo",
+    "ver": "ver"
+}
