2b195a1635263fb3714344fa2766d4ddfbb1cc34
Bart Thate <rssbotd@gmail.com>
2026-09-11 20:15:51 +0200

    add modules

---
+++ nixt/buffers.py
@@ -0,0 +1,96 @@
+# This file is placed in the Public Domain.
+
+
+"buffered output"
+
+
+import logging
+import queue
+import threading
+import _thread
+
+
+from .brokers import Broker
+from .engines import Engine
+from .threads import Thread
+
+
+logger = logging.getLogger(__name__)
+
+
+class Output:
+
+    "dedicated output loop"
+
+    def __init__(self):
+        self.oqueue = queue.Queue()
+        self.ostopped = threading.Event()
+
+    def display(self, event):
+        "do actual display."
+
+    def output(self):
+        "output loop."
+        while not self.ostopped.is_set():
+            try:
+                event = self.oqueue.get()
+            except (KeyboardInterrupt, EOFError):
+                _thread.interrupt_main()
+            if event is None:
+                self.oqueue.task_done()
+                break
+            self.display(event)
+            self.oqueue.task_done()
+
+    def raw(self, text):
+        "raw output."
+        raise NotImplementedError
+
+    def start(self, daemon=True):
+        "start output loop."
+        self.ostopped.clear()
+        Thread.launch(self.output, daemon=daemon)
+
+    def stop(self):
+        "stop output loop."
+        self.ostopped.set()
+        self.oqueue.put(None)
+
+    def wait(self):
+        "wait for output to finish."
+        try:
+            self.oqueue.join()
+        except Exception:
+            logger.exception()
+            _thread.interrupt_main()
+
+
+class Buffer(Engine, Output):
+
+    "buffered output"
+
+    def __init__(self):
+        Engine.__init__(self)
+        Output.__init__(self)
+        Broker.add(self)
+
+    def raw(self, text):
+        "raw output."
+        raise NotImplementedError
+
+    def start(self, daemon=True):
+        "start output loop."
+        Engine.start(self)
+        Output.start(self, daemon=daemon)
+
+    def stop(self):
+        "stop output loop."
+        Engine.stop(self)
+        Output.stop(self)
+
+
+def __dir__():
+    return (
+        'Buffer',
+        'Output'
+    )
---
+++ nixt/display.py
@@ -0,0 +1,70 @@
+# This file is placed in the Public Domain.
+
+
+"an object for a string"
+
+
+import threading
+
+
+from .brokers import Broker
+from .engines import Engine
+
+
+class Display:
+
+    "unit of display"
+
+    block = threading.Event()
+
+    def __init__(self):
+        self.olock = threading.RLock()
+        self.silent = False
+        
+    def announce(self, text):
+        "announce text to all channels"
+        if not self.silent:
+            self.raw(text)
+
+    def display(self, event):
+        "display event results"
+        with self.olock:
+            for txt in event.result:
+                if self.block.is_set():
+                    return
+                self.dosay(event.channel, txt)
+                del txt
+        del event
+
+    def dosay(self, channel, text):
+        "say called by display"
+        self.say(channel, text)
+
+    def raw(self, text):
+        "raw output"
+        raise NotImplementedError
+
+    def say(self, channel, text):
+        "say text in channel"
+        self.raw(text)
+
+
+class Screen(Engine, Display):
+
+    "display wit coupled handler"
+
+    def __init__(self):
+        Engine.__init__(self)
+        Display.__init__(self)
+        Broker.add(self)
+
+    def raw(self, text):
+        "raw output."
+        raise NotImplementedError
+
+
+def __dir__():
+    return (
+        'Display',
+        'Screen'
+    )
