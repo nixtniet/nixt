6eab3de6b28da0ee3d398791fe2cec42e4869412
Bart Thate <rssbotd@gmail.com>
2026-09-11 23:20:22 +0200

    skip the F401

--- nixt/defines.py
+++ nixt/defines.py
@@ -1,45 +1,45 @@
 # This file is placed in the Public Domain.
-# ruff: noqa: F403,F405,PLC0414,RUF100,PLE0605
+# ruff: noqa: F403,F401,F405,PLC0414,RUF100,PLE0605
 
 
 "interface"
 
 
-from .booting import Boot as Boot
-from .brokers import Broker as Broker
-from .buffers import Buffer as Buffer
-from .buffers import Output as Output
-from .clients import Clients as Clients
-from .command import Commands as Commands
-from .configs import Main as Main
-from .display import Display as Display
-from .display import Screen as Screen
-from .encoder import JSON as JSON
-from .encoder import JSONL as JSONL
-from .engines import Engine as Engine
-from .fetcher import Fetcher as Fetcher
-from .loggers import Format as Format
-from .loggers import Logging as Logging
-from .looping import Loop as Loop
-from .message import Message as Message
-from .methods import Method as Method
-from .objects import Data as Data
-from .objects import Object as Object
-from .package import Mods as Mods
-from .parsers import Parser as Parser
-from .persist import Disk as Disk
-from .persist import Locater as Locater
-from .persist import Workdir as Workdir
-from .pooling import Pool as Pool
-from .require import Cmd as Cmd
-from .repeats import Repeater as Repeater
-from .runners import Runner as Runner
-from .sources import MD5 as MD5
-from .threads import Thr as Thr
-from .threads import Thread as Thread
-from .timings import Time as Time
-from .utility import Utils as Utils
-from .watcher import Watcher as Watcher
+from .booting import Boot
+from .brokers import Broker
+from .buffers import Buffer
+from .buffers import Output
+from .clients import Clients
+from .command import Commands
+from .configs import Main
+from .display import Display
+from .display import Screen
+from .encoder import JSON
+from .encoder import JSONL
+from .engines import Engine
+from .fetcher import Fetcher
+from .loggers import Format
+from .loggers import Logging
+from .looping import Loop
+from .message import Message
+from .methods import Method
+from .objects import Data
+from .objects import Object
+from .package import Mods
+from .parsers import Parser
+from .persist import Disk
+from .persist import Locater
+from .persist import Workdir
+from .pooling import Pool
+from .require import Cmd
+from .repeats import Repeater
+from .runners import Runner
+from .sources import MD5
+from .threads import Thr
+from .threads import Thread
+from .timings import Time
+from .utility import Utils
+from .watcher import Watcher
 
 
 def __dir__():
