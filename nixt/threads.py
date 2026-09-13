4212c25138df7d1bed02fb84faba5ca2f20a7b1c
Bart Thate <rssbotd@gmail.com>
2026-09-13 03:02:14 +0200

    linters

--- nixt/threads.py
+++ nixt/threads.py
@@ -54,8 +54,8 @@ class Thr(threading.Thread):
             self.result = func(*args)
         except (KeyboardInterrupt, EOFError):
             _thread.interrupt_main()
-        except Exception as ex:
-            logger.exception(ex)
+        except Exception:
+            logger.exception()
             _thread.interrupt_main()
 
 
